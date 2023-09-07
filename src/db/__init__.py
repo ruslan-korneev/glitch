76308e9381d8841b43e9c675f8f632c640198eb5
Ruslan <shaggybackend@gmail.com>
2023-09-07 21:37:30 +0200

    core(#14)[db]: move crud to abstract alchemy model, remove single session and use async_generator instead

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -5,12 +5,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.config.db import Base, SessionLocal
 from src.db import models
 
-single_async_session: AsyncSession = SessionLocal()
 
-
-async def get_async_session() -> AsyncGenerator[AsyncSession, Any]:
+async def get_session() -> AsyncGenerator[AsyncSession, Any]:
     async with SessionLocal() as session:
         yield session
 
-
-__all__ = ["single_async_session", "Base", "models"]
+__all__ = ["Base", "models", "get_session"]
