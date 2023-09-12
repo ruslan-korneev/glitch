178ef783490838e86bb20bd0a8ece3bae76941c2
bezzyd <usenko.alexandr97@mail.ru>
2023-09-12 16:26:46 +0300

    wip: enhancement(#11)[timetracking]: create functions for get projects, user, spent time

--- src/services/gitlab/timetracker.py
+++ src/services/gitlab/timetracker.py
@@ -2,41 +2,52 @@ from datetime import datetime, timedelta
 from enum import auto
 
 from gitlab import Gitlab
-from gitlab.const import MINIMAL_ACCESS
+from gitlab.const import AccessLevel
 from strenum import LowercaseStrEnum
-
-# MINIMAL_ACCESS.GUEST
 
 
 class TimeTracker:
-    # gitlab = Gitlab(oauth=user, get_gitlab_access_token())
-
-    def __init__(self, project_id: int, user_id: int, gitlab: Gitlab):
-        self.project_id = project_id
-        self.username = user_id
+    def __init__(self, gitlab: Gitlab) -> None:
         self.gitlab = gitlab
 
-    def get_project(self):
-        project = self.gitlab.projects.get(self.project_id)
-        return project
+    def get_project_list(self):
+        return self.gitlab.projects.list(min_access_level=AccessLevel.GUEST)
 
-    def get_user(self):
-        user = self.gitlab.users.get(self.user_id)
-        return user
+    def get_project(self, project_id: int):
+        return self.gitlab.projects.get(project_id)
 
-    def get_time_by_project(self):
-        """
-        Method to get list of spent time for issues
-        and merge-requests of certain project
-        """
-        return self.get_project().issues.list().mergerequest.list().asdict()
+    def get_current_user(self):
+        return self.gitlab.user
 
-    def get_time_by_user(self):
+    def get_spent_time_for_issues_by_project(self, project_id: int):
         """
-        Method to get list of spent time for issues
-        and merge-requests by certain user
+        Method to get list of spent time
+        for issues by certain project
         """
-        return self.get_user().issues.list().mergerequest.list().asdict()
+        return self.get_project(project_id).issues.list()
+
+    def get_spent_time_for_mergerequests_by_project(self, project_id: int):
+        """
+        Method to get list of spent time for
+        merge-requests by certain project
+        """
+        return self.get_project(project_id).mergerequests.list()
+
+    # ===================================================================================
+
+    def get_spent_time_for_issues_by_user(self):
+        """
+        Method to get list of spent time
+        for issues by certain user
+        """
+        return self.gitlab.user.issues.list()
+
+    def get_spent_time_for_mergerequests_by_user(self):
+        """
+        Method to get list of spent time for
+        merge-requests by certain user
+        """
+        return self.gitlab.user.mergerequest.list()
 
     def get_time_by_user_and_project(self):
         """
